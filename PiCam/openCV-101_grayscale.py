f4778025878a134a33c32aeebb22c28ca41a6208
ronk62 <ronk62@gmail.com>
2020-09-17 05:18:14 +0000

    PiCam and openCV tests and examples

---
+++ PiCam/openCV-101_grayscale.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# 
+
+'''
+from site: https://www.instructables.com/id/Automatic-Vision-Object-Tracking/
+
+'''
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+ 
+while(True):
+    ret, frame = cap.read()
+    # frame = cv2.flip(frame, -1) # Flip camera vertically
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # cv2.imshow('frame', frame)
+    cv2.imshow('gray', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
