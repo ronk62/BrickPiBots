049ecec296b7a038fd36cc59977058391ca0df89
ronk62 <ronk62@gmail.com>
2020-09-17 02:35:05 +0000

    simplified to just one channel and mode (IR-Seek)

--- IRsensorProxSeekTestV0.1.py
+++ IRsensorProxSeekTestV0.1.py
@@ -11,10 +11,10 @@ import time, tty, sys
 from ev3dev2 import list_devices
 from ev3dev2.port import LegoPort
 from ev3dev2.motor import OUTPUT_A, LargeMotor, SpeedPercent
-from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor import INPUT_3
 from ev3dev2.sensor.lego import InfraredSensor
 
-p1 = LegoPort(INPUT_1)
+p1 = LegoPort(INPUT_3)
 # http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/brickpi3.html#brickpi3-in-port-modes
 p1.mode = 'ev3-uart'
 # http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/sensors.html#supported-sensors
@@ -22,7 +22,7 @@ p1.set_device = 'lego-ev3-ir'
 
 
 # Connect infrared to any sensor port
-ir = InfraredSensor(INPUT_1)
+ir = InfraredSensor(INPUT_3)
 
 # allow for some time to load the new drivers
 time.sleep(0.5)
@@ -44,14 +44,16 @@ startTime = time.time()
 
 while (time.time() - startTime < 600):
     ir.mode = 'IR-SEEK'
-    irHeadVal = ir.value(0)       ### value(0) is the heading value in this mode
-    irDistVal = ir.value(1)       ### value(1) is the distance value in this mode
+    irHeadVal = ir.value(0)       ### value(0) is the channel-1 heading value in this mode
+    irDistVal = ir.value(1)       ### value(1) is the channel-1 distance value in this mode
+    # irHeadVal = ir.heading(channel=1)   ### value(0) is the heading value in this mode
+    # irDistVal = ir.distance(channel=1)  ### value(1) is the distance value in this mode
 
-    if (irDistVal < 0) or (irDistVal > 99):
-        print("Dropped into IR-PROX mode")
-        ir.mode = 'IR-PROX'
-        irProxVal = ir.value(0)   ### value(0) is the proximity value in this mode
-        time.sleep(0.2)
+    # if (irDistVal < 0) or (irDistVal > 99):
+    #     print("Dropped into IR-PROX mode")
+    #     ir.mode = 'IR-PROX'
+    #     irProxVal = ir.value(0)   ### value(0) is the proximity value in this mode
+    #     time.sleep(0.2)
     
     if (irProxVal != prev_irProxVal) or (irDistVal != prev_irDistVal) or (irHeadVal != prev_irHeadVal):
             print("irProxVal, irDistVal, irHeadVal  ", irProxVal, irDistVal, irHeadVal)
