2064f38680819868db9e50c379d56aa2181abf33
ronk62 <ronk62@gmail.com>
2021-04-17 01:58:12 +0000

    showing cam/bot in base-frame (newest model)

--- PiCam/apriltag-102-LivePlot.py
+++ PiCam/apriltag-102-LivePlot.py
@@ -21,12 +21,17 @@ and https://github.com/swatbotics/apriltag
 # pastable into Python:
 #   fx, fy, cx, cy = (604.8851295863385, 606.0410127799453, 320.0, 240.0)
 
+# 4/16/2021     - incorperated approach for rotating the tag to align with world frame coords developed
+#                 in  apriltag-102.py
+#               - still plotting only the raw values from the matrix (not the cartesian unit vector)
+#
+
 
 import numpy as np
 import math
 import cv2
 import apriltag
-import time, tty, sys, threading
+import time, os, tty, sys, threading
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
@@ -38,6 +43,76 @@ ii = 0
 
 tagInCamFrame = np.array([[],[],[],[]], dtype=np.int32)
 camInTagFrame = np.array([[],[],[],[]], dtype=np.int32)
+
+#np.set_printoptions(precision=2,floatmode='fixed')
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+# vars for rotation matrices and calculations
+R0_1 = np.array([], dtype=np.int32)
+d0_1 = np.array([], dtype=np.int32)
+
+H0 = np.array([], dtype=np.int32)
+H1 = np.array([], dtype=np.int32)
+H0_1 = np.array([], dtype=np.int32)
+H0_2 = np.array([], dtype=np.int32)
+
+# H0 at origin
+H0 = [[1,0,0,0],
+[0,1,0,0],
+[0,0,1,0],
+[0,0,0,1]]
+
+# # H0 NOT at origin
+# H0 = [[1,0,0,0],
+# [0,1,0,400],
+# [0,0,1,0],
+# [0,0,0,1]]
+
+
+# H1 displaced from origin with no rotation - for ref only
+H1 = [[1,0,0,0.05],
+[0,1,0,0],
+[0,0,1,0.05],
+[0,0,0,1]]
+
+# Assign Eurler rotation angles
+theta1Deg=0  # y rotation angle between H0 and H1 (positive values are cw when viewed top down in real world)
+
+# convert angles from deg to radians
+theta1Rad=math.radians(theta1Deg)
+
+# Assign displacement values
+ax1=0     # x displacement between H0 and H1 in meters
+az1=0     # z displacement between H0 and H1 in meters
+
+
+### Define the rotation
+# R0_1
+R0_1 = [[np.cos(theta1Rad),0,np.sin(theta1Rad)],
+[0,1,0],
+[-np.sin(theta1Rad),0,np.cos(theta1Rad)]]
+
+
+# Apply the displacement translations
+d0_1 = [[ax1],[0],[az1]]
+
+H0_1 = np.concatenate((R0_1, d0_1), 1)
+H0_1 = np.concatenate((H0_1, [[0,0,0,1]]), 0)
+
+H0_1 = np.dot(H0, H0_1)
+
+print()
+print("H0_1 is  ")
+print(np.matrix(H0_1))
+print()
+
+# extract the values from H0_1
+x1 = H0_1[0][3]
+y1 = H0_1[2][3]
+
+print()
+print("the x,y position of H0_1 is  ", x1,y1)
+print()
 
 Xx = np.array([], dtype=np.int32)
 Xy = np.array([], dtype=np.int32)
@@ -73,6 +148,7 @@ def animate(i):
     global cap
     global tagInCamFrame
     global camInTagFrame
+    global H0_1, H0_2
     global Xx, Xy, Xz
     global Yx, Yy, Yz
     global Zx, Zy, Zz
@@ -124,6 +200,18 @@ def animate(i):
                 print(np.matrix(camInTagFrame))
                 print("")
 
+                # calculate and print cam/bot in base frame
+                H0_2 = np.dot(H0_1, camInTagFrame)
+
+                print("")
+                print("cam/bot in base frame (H0_2) pose is... ")
+                print(np.matrix(H0_2))
+                print("")
+
+                # extract the values from H0_2 for graphing
+                x2 = H0_2[0][3]
+                y2 = H0_2[2][3]
+
                 # calculate and print Robot heading (Z) Euler angle from Y axis rotation
                 # column 3, row 1
                 print("")
@@ -133,25 +221,26 @@ def animate(i):
                 print(ZxDeg, "  degrees")
                 print("")
 
-                # column 1
-                Xx = np.append(Xx, camInTagFrame[0][0])
-                Xy = np.append(Xy, camInTagFrame[1][0])
-                Xz = np.append(Xz, camInTagFrame[2][0])
+                # extract the values from H0_2 for graphing
+                ## column 1
+                Xx = np.append(Xx, H0_2[0][0])
+                Xy = np.append(Xy, H0_2[1][0])
+                Xz = np.append(Xz, H0_2[2][0])
                 
-                # column 2
-                Yx = np.append(Yx, camInTagFrame[0][1])
-                Yy = np.append(Yy, camInTagFrame[1][1])
-                Yz = np.append(Yz, camInTagFrame[2][1])
+                ## column 2
+                Yx = np.append(Yx, H0_2[0][1])
+                Yy = np.append(Yy, H0_2[1][1])
+                Yz = np.append(Yz, H0_2[2][1])
                 
-                # column 3
-                Zx = np.append(Zx, camInTagFrame[0][2])
-                Zy = np.append(Zy, camInTagFrame[1][2])
-                Zz = np.append(Zz, camInTagFrame[2][2])
+                ## column 3
+                Zx = np.append(Zx, H0_2[0][2])
+                Zy = np.append(Zy, H0_2[1][2])
+                Zz = np.append(Zz, H0_2[2][2])
                 
-                # column 4
-                Xt = np.append(Xt, camInTagFrame[0][3])
-                Yt = np.append(Yt, camInTagFrame[1][3])
-                Zt = np.append(Zt, camInTagFrame[2][3])
+                ## column 4
+                Xt = np.append(Xt, H0_2[0][3])
+                Yt = np.append(Yt, H0_2[1][3])
+                Zt = np.append(Zt, H0_2[2][3])
 
         #append the independant array then increment ii
         ODt = np.append(ODt, ii)
