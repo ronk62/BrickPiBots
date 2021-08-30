af3516cf298abc72e4625870cd74fd2c9e174375
ronk62 <ronk62@gmail.com>
2021-08-30 06:53:26 +0000

    initial code aimed at measuring compass accuracy

--- BrickPiDiffDrvSelfDriving/BPDDwasdOdomCmpsGyroV20210814.py
+++ BrickPiDiffDrvSelfDriving/BPDDwasdOdomCmpsGyroV20210814.py
@@ -8,7 +8,9 @@
 #                           - starting with circular routes - simple cw pivot turns with only the left motor
 #                             running to capture and compare measurements from compass, gyro, motor-tics
 #
-#
+# 8/29/2021     Ron King    - created functions for reading the sensors
+#                           - added data collection and plotting routines
+#                            
 
 """ 
 This program provides SLAM testing with wasd keyboard remote control and data collection.
@@ -21,12 +23,13 @@ motor B = Right motor
  """
 
 import time, tty, sys, threading
+import matplotlib.pyplot as plt
 import numpy as np
 from ev3dev2 import list_devices
 from ev3dev2.port import LegoPort
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, SpeedDPS, MoveTank
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
-from ev3dev2.sensor.lego import InfraredSensor, UltrasonicSensor
+from ev3dev2.sensor.lego import InfraredSensor, UltrasonicSensor, GyroSensor
 from ev3dev2.sensor.lego import Sensor
 
 
@@ -98,17 +101,64 @@ ir = InfraredSensor(INPUT_3)
 # allow for some time to load the new drivers
 time.sleep(0.5)
 
-# irProxVal = 100
-# prev_irProxVal = 0
-irDistVal = 0
-prev_irDistVal = 0
-irHeadVal = 0
-prev_irHeadVal = 0
+irProxVal = 100
+prev_irProxVal = 0
+
+
+
+p4 = LegoPort(INPUT_4)
+
+# http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/brickpi3.html#brickpi3-in-port-modes
+p4.mode = 'ev3-uart'
+
+# allow for some time for mode to setup
+time.sleep(0.5)
+
+# http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/sensors.html#supported-sensors
+
+""" https://github.com/ev3dev/ev3dev/issues/640
+
+dlech commented on May 15, 2016
+    You are forgetting the I2C address for set_device
+
+$ echo mx-pps58-nx 0x0C >/sys/class/lego-port/port0/set_device 
+
+dmesg -w
+
+"""
+
+# set device name and i2c address (in hex)
+p4.set_device = 'lego-ev3-gyro'
+
+# allow for some time for setup
+time.sleep(0.5)
+
+# Connect sensor to sensor port 4
+gyro = GyroSensor(INPUT_4)
+
+# allow for some time to load the new drivers
+time.sleep(0.5)
+
+print("")
+print("")
+print("Initializing Gyro - Don't move the robot for a few seconds ")
+print("")
+
+#gyro.mode = 'GYRO-ANG'
+
+gyro.mode = 'GYRO-FAS'
+
+gyroVal = 0
+prev_gyroVal = -1
+
+time.sleep(2)     # give some time to stabilize gyro before reading values
+
+
 
 
 # keyboard control setup and vars
 tty.setcbreak(sys.stdin)
-x = 0   # set here to make global
+kx = 0   # set here to make global
 
 # Nav control vars
 sample_mode = -1        # start in sample mode off, '-1'; '1' means sample mode is enabled
@@ -122,18 +172,31 @@ navPrint = 10           # print the Nav msgs every nth cycle
 printVerbose = 1        # toggle verbose data/msg printing to terminal (-1 is 'disabled')
 i = 0                   # "outer" iterator for dist point sampling
 j = 0                   # "inner" iterator for dist point sampling
-prev_mRposition = 0     # var to help detect when to save a new OD pair (motor pos, cmpVal) to array or print to screen
+# prev_mRposition = 0     # var to help detect when to save a new OD pair (motor pos, cmpVal) to array or print to screen
+badValCount = 0
 
-# SLAM vars
+
+### SLAM vars ###
 cmpDegVal = np.array([], dtype=np.int32)            # arrary to hold compass-angle  in degrees
+
 # added Gyro
 gyroDegVal = np.array([], dtype=np.int32)           # arrary to hold gyro-angle  in degrees
+
 # added array motor encoder ticks for odometry
 ODmLencoderVal = np.array([], dtype=np.int32)       # arrary to hold left motor encoder-count while moving
-ODmRencoderVal = np.array([], dtype=np.int32)       # arrary to hold right motor encoder-count while moving
+#ODmRencoderVal = np.array([], dtype=np.int32)       # arrary to hold right motor encoder-count while moving
+
+# added array to hold calculated robot rotation deg based on motor ticks
+ODmLrotDeg = np.array([], dtype=np.int32)           # arrary to hold left motor calculated dist (cm)
+
 # added another array for compass odometry
 ODcmpDeg = np.array([], dtype=np.int32)             # array to hold associated compass angle (degrees) for each motor tick
+
+
 # for later expansion
+# added array to hold calculated motor distance values 
+#ODmLdistCm = np.array([], dtype=np.int32)           # arrary to hold left motor calculated dist (cm)
+#ODmRdistCm = np.array([], dtype=np.int32)           # arrary to hold right motor calculated dist (cm)
 #polarCoordsUSr = np.array([], dtype=np.int32)       # arrary to hold 'r' - US distance r (in cm)
 #polarCoordsIRr = np.array([], dtype=np.int32)       # arrary to hold 'r' - IR distance r (in arb units)
 
@@ -147,6 +210,8 @@ mLspd = 0
 mL.stop_action = 'coast'
 # mL.polarity = 'inversed'
 mL.position = 0
+
+ODmLrotDegVal = 0
  
 
 mR = LargeMotor(OUTPUT_B)
@@ -168,55 +233,62 @@ def keyboardInput(name):
         kx = ord(sys.stdin.read(1))
         time.sleep(0.05)
 
+def readEV3usDistCm():
+    global usDistCmVal, prev_usDistCmVal
+    usDistCmVal = us.distance_centimeters
+    # work-around for bogus intermittent readings of '0' (possibly a f/w bug in BrickPi3)
+    if usDistCmVal == 0 and abs(usDistCmVal - prev_usDistCmVal) > 1:
+        usDistCmVal = prev_usDistCmVal
+    if usDistCmVal != prev_usDistCmVal:
+        # print("usDistCmVal = ", usDistCmVal)
+        prev_usDistCmVal = usDistCmVal
+
+def readHTcompass():
+    global compassVal, prev_compassVal
+    compassVal = cmp.value(0)
+    if compassVal != prev_compassVal:
+        # print("compassVal = ", compassVal)
+        prev_compassVal = compassVal
+
+def readEV3irProx():
+    global irProxVal, prev_irProxVal
+    irProxVal = ir.proximity
+    # work-around for bogus intermittent readings of '0' (possibly a f/w bug in BrickPi3)
+    if irProxVal == 0 and abs(irProxVal - prev_irProxVal) > 1:
+        irProxVal = prev_irProxVal
+    if irProxVal != prev_irProxVal:
+        # print("irProxVal = ", irProxVal)
+        prev_irProxVal = irProxVal
+
+def readEV3gyro():
+    global gyroVal, prev_gyroVal
+    gyroVal = gyro.angle
+    # work-around for bogus intermittent readings of '0' (possibly a f/w bug in BrickPi3)
+    if gyroVal == 0 and abs(gyroVal - prev_gyroVal) > 1:
+        gyroVal = prev_gyroVal
+    if gyroVal != prev_gyroVal:
+        # print("gyroVal = ", gyroVal)
+        prev_gyroVal = gyroVal
+
 
 if __name__ == "__main__":
     myThread = threading.Thread(target=keyboardInput, args=(1,), daemon=True)
     myThread.start()
     print ("Ready for keyboard commands...")
+    print("")
 
 
 ### Main Loop
     while (True):
+        # read the sensors
+        readEV3usDistCm()
+        readHTcompass()
+        readEV3irProx()
+        readEV3gyro()
 
-'''
-        replace this next section with something newer - perhaps from apriltag stuff
+        # print all sensor values (perhaps define a var 'senseChange' and set to 0 or 1)
+        print("usDistCmVal = ", int(usDistCmVal), "  compassVal = ", compassVal, "  irProxVal = ", irProxVal, "  gyroVal = ", gyroVal) 
 
-        usDistCmVal = us.distance_centimeters
-        if usDistCmVal != prev_usDistCmVal:
-            if printVerbose > 0:
-                print("usDistCmVal = ", int(usDistCmVal), "  irDistVal = ", irDistVal, "  irHeadVal = ", irHeadVal, "  compassVal = ", compassVal, "  mR.position = ", mR.position)  # print all sensor vals, regardless of which changed
-            prev_usDistCmVal = usDistCmVal
-
-        irDistVal = ir.distance()
-        irHeadVal = ir.heading()
-        if (irDistVal != prev_irDistVal):
-            if printVerbose > 0:
-                print("usDistCmVal = ", int(usDistCmVal), "  irDistVal = ", irDistVal, "  irHeadVal = ", irHeadVal, "  compassVal = ", compassVal, "  mR.position = ", mR.position)  # print all sensor vals, regardless of which changed
-            prev_irDistVal = irDistVal
-            prev_irHeadVal = irHeadVal
-
-
-        compassVal = cmp.value(0)
-        if compassVal != prev_compassVal:
-            # print("usDistCmVal = ", usDistCmVal, "  irDistVal = ", irDistVal, "  irHeadVal = ", irHeadVal, "  compassVal = ", compassVal)  # print all sensor vals, regardless of which changed
-            if printVerbose > 0:
-                print("usDistCmVal = ", int(usDistCmVal), "  irDistVal = ", irDistVal, "  irHeadVal = ", irHeadVal, "  compassVal = ", compassVal, "  mR.position = ", mR.position)  # print all sensor vals, regardless of which changed
-            prev_compassVal = compassVal
-            # if beaconLock is False and we're outside the compass North range, reset cmpGenHeading to False
-            if compassVal < (compassGoal - 80) or compassVal > (compassGoal + 80):
-                cmpGenHeading = False
-
-
-        keep and merge this next section 
-
-        if mR.position != prev_mRposition:
-            prev_mRposition = mR.position
-            if printVerbose > 0:
-                print("usDistCmVal = ", int(usDistCmVal), "  irDistVal = ", irDistVal, "  irHeadVal = ", irHeadVal, "  compassVal = ", compassVal, "  mR.position = ", mR.position)  # print all sensor vals, regardless of which changed
-
-'''
-
-        ### if usDistCmVal < 5:
         if usDistCmVal < 5:    ### changed from '10' to '5' for US data accuracy testing
             if obstacleNear == False:
                 print("Obstacle Detected! Forward motion stopped.")
@@ -244,34 +316,54 @@ if __name__ == "__main__":
             printVerbose *= -1
             print("toggled printVerbose mode (v)")
 
-        # if kx == 102: # f pushed - save sample data to files
-        #     print("")
-        #     if gyroDegVal.size < 1 or cmpDegVal.size < 1 or ODmLencoderVal.size < 1 or ODmRencoderVal.size < 1 or ODcmpDeg.size < 1:
-        #         print("no data at this time")
-        #     else:
-        #         print ("saving sample data to csv files")
-        #         # save to csv files
-        #         # np.savetxt('polarCoordsUSrV20210814.csv', polarCoordsUSr, delimiter=',')
-        #         # np.savetxt('polarCoordsIRrV20210814.csv', polarCoordsIRr, delimiter=',')
-        #         np.savetxt('ODmLencoderValV20210814.csv', ODmLencoderVal, delimiter=',')
-        #         np.savetxt('ODmRencoderValV20210814.csv', ODmRencoderVal, delimiter=',')
-        #         np.savetxt('gyroDegValV20210814.csv', gyroDegVal, delimiter=',')
-        #         np.savetxt('cmpDegValV20210814.csv', cmpDegVal, delimiter=',')
-        #         print ("saves commplete")
-        #         print("")
-
 
         if kx == 105: # i pushed - toggle sample mode on and off
             sample_mode *= -1
             print("toggled sample_mode (i); now set to...  ", sample_mode)
         
+
+        ## some notes about odometry and robot wheelbase
+        # from V501b-9.csv
+        #      --> average of 18.452752525 ticks per cm
+        #      --> average of 0.054192457 cm per tick
+        # robot wheelbase = 14 cm (this is the radius of a pivot turn on one wheel)
+        # 2 pi r = 88 cm (circumference, i.e. full robot rotation) = 1624 motor ticks
+        # 4.5 ticks per degree of robot rotation
+        # a 45 deg robot pivot turn = 202 motor ticks
+        # a 30 deg robot pivot turn = 135 motor ticks
+        # a 10 deg robot pivot turn = 45 motor ticks
+        
         if sample_mode > 0:
             try:
+                
+                # Make sure motors are initially stopped
+                mL.on(0, brake=True)
+                mR.on(0, brake=True)
+                
+                # Make sure the robot is oriented (x,y and theta) as desired before continuing
+                print("")
+                print("Make sure the robot is oriented (x,y and theta) as desired before continuing")
+                print("")
+                time.sleep(0.3)
+
+                if compassVal >= 2:
+                    print("Compass value should be 0 for start of this test, but was found to be...")
+                    print("compassVal = ", compassVal)
+                    print("")
+                    print("Exiting sample mode")
+                    print("")
+                    sample_mode = -1
+                    print("toggled sample_mode (i); now set to...  ", sample_mode)
+                    print("")
+                    time.sleep(3)
+                    break
+
+                
                 # init the data collection arrays for each sample collection
                 cmpDegVal = np.array([], dtype=np.int32)
                 gyroDegVal = np.array([], dtype=np.int32)
                 ODmLencoderVal = np.array([], dtype=np.int32)
-                ODmRencoderVal = np.array([], dtype=np.int32)
+                ODmLrotDeg = np.array([], dtype=np.int32)
                 ODcmpDeg = np.array([], dtype=np.int32)
 
                 # set motor encoders to 0
@@ -280,57 +372,83 @@ if __name__ == "__main__":
                 mR.position = 0
                 prev_mRposition = 0
 
-                # take samples while stopped - increment translation/rotation
-                while usDistCmVal > 20:      ## FIX ME
-                    #mL.on(5, brake=False)
-                    mR.on(5, brake=False)
-                    usDistCmVal = us.distance_centimeters
-                    irDistVal = ir.distance()
-                    if irDistVal == None:
-                        irDistVal = -1      ### set to -1 instead of None or numpy.savetxt will complain
-                    compassVal = cmp.value(0)
+                ### move motors, then take samples while stopped
+                # for i in range(3,359,3):
+                #     # rotate robot 3 deg cw
+                #     print("rotating robot 3 deg cw, then pausing for 1 sec") 
+                #     mL.on_for_degrees(speed=3, degrees= 3 * 4.5)
+
+                # for quick testing
+                for i in range(10,359,10):
+                    # rotate robot 10 deg cw
+                    print("rotating robot 3 deg cw, then pausing for 1 sec; i = ", i) 
+                    mL.on_for_degrees(speed=3, degrees= 10 * 4.5)
+                    
+                    # pause to let robot top shaking
+                    time.sleep(1)
+                    
+                    # read the sensors
+                    readEV3usDistCm()
+                    readHTcompass()
+                    readEV3irProx()
+                    readEV3gyro()
+                    ODmLrotDegVal = mL.position / 4.5
+                    print("usDistCmVal = ", int(usDistCmVal), "  compassVal = ", compassVal, "  irProxVal = ", irProxVal, "  gyroVal = ", gyroVal, "mL.position = ", mL.position) 
+
+                    # append the arrays
                     cmpDegVal = np.append(cmpDegVal, compassVal)
-                    gyroDegVal = np.append(gyroDegVal, UPDATE WITH GYRO READING)  ## FIX ME
+                    gyroDegVal = np.append(gyroDegVal, gyroVal)
                     ODmLencoderVal = np.append(ODmLencoderVal, mL.position)
-                    ODmRencoderVal = np.append(ODmRencoderVal, mR.position)
+                    ODmLrotDeg = np.append(ODmLrotDeg, ODmLrotDegVal)
                     ODcmpDeg = np.append(ODcmpDeg, compassVal)
-                    if cmpDegVal.size > 100000:
+                    
+                    if cmpDegVal.size > 1000:
                         sample_mode = -1
                         print("")
                         print("")
                         print ("Exiting sample mode due to sample size too large...")
                         print ("gyroDegVal.size = ", gyroDegVal.size)
                         print("")
-                    time.sleep(.05)             
-                mL.on(0, brake=True)
-                mR.on(0, brake=True)
-                print("")
-                print ("gyroDegVal.size = ", gyroDegVal.size)
-                print("")
-                print ("gyroDegVal = ", gyroDegVal)
-                print("")
-                print ("ODmLencoderVal.size = ", ODmLencoderVal.size)
-                print("")
-                print ("ODmLencoderVal = ", ODmLencoderVal)
-                print("")
-                print ("ODmRencoderVal.size = ", ODmRencoderVal.size)
-                print("")
-                print ("ODmRencoderVal = ", ODmRencoderVal)
-                print("")
-                print ("ODcmpDeg.size = ", ODcmpDeg.size)
-                print("")
-                print ("ODcmpDeg = ", ODcmpDeg)
-                print("")
-                print ("cmpDegVal.size = ", cmpDegVal.size)
-                print("")
-                print ("cmpDegVal = ", cmpDegVal)
-                print("")
-                print("")
+                    
+                    time.sleep(0.3)             
+                
                 
             except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
                 mL.on(0, brake=False)
                 mR.on(0, brake=False)
                 sample_mode = -1
+
+
+            # Make sure motors are stopped
+            mL.on(0, brake=True)
+            mR.on(0, brake=True)
+            
+            ### Graph the data
+            print("")
+            print("creating graphs...")
+            print("")
+            
+            # plt.plot(cmpDegVal,ODmLencoderVal, label='ODmLencoderVal')
+            # plt.plot(cmpDegVal,gyroDegVal, label='gyroDegVal')
+            # plt.plot(cmpDegVal,ODmLrotDeg, label='ODmLrotDeg')
+            # plt.xlabel('HTcompass-EV2Gyro-MotorTicks-Deg')
+            # plt.ylabel('sensor data')
+            # plt.title('Odometry data')
+            # plt.legend()
+            # plt.show()
+
+            # plt.scatter(cmpDegVal,ODmLencoderVal, label='ODmLencoderVal', color='r', s=10, marker="o")
+            # plt.scatter(cmpDegVal,gyroDegVal, label='gyroDegVal', color='b', s=10, marker="o")
+            # plt.scatter(cmpDegVal,ODmLrotDeg, label='ODmLrotDeg', color='k', s=10, marker="o")
+            plt.scatter(ODmLrotDeg,ODmLencoderVal, label='ODmLencoderVal', color='r', s=10, marker="o")
+            plt.scatter(ODmLrotDeg,gyroDegVal, label='gyroDegVal', color='b', s=10, marker="o")
+            plt.scatter(ODmLrotDeg,cmpDegVal, label='cmpDegVal', color='k', s=10, marker="o")
+            plt.axis('equal')
+            plt.xlabel('ODmLrotDeg vs ODmLencoderVal-EV2Gyro-HTcompass')
+            plt.ylabel('sensor data')
+            plt.title('Odometry data')
+            plt.legend()
+            plt.show()
 
             sample_mode = -1
             print("Sampling complete. Returning to keyboard cmd mode.")
@@ -353,12 +471,13 @@ if __name__ == "__main__":
         if kx == 120: # x key means exit
             break
         
-        if navPrint == 10:      ## FIX ME
-            # print("NAV:  x, spd, turnRatio, pilot_mode, beaconSearch, cmpSearch, cmpGenHeading, beaconLock  ", x, spd, turnRatio, pilot_mode, beaconSearch, cmpSearch, cmpGenHeading, beaconLock)
-            # print("NAV:  x, spd, turnRatio, pilot_mode, searchSLAM1, cmpSearch, cmpGenHeading, beaconLock  ", x, spd, turnRatio, pilot_mode, searchSLAM1, cmpSearch, cmpGenHeading, beaconLock)
-            if printVerbose > 0:
-                print("NAV:  x, spd, turnRatio, sample_mode, searchSLAM1, cmpSearch, cmpGenHeading  ", x, spd, turnRatio, sample_mode, searchSLAM1, cmpSearch, cmpGenHeading)
+        if printVerbose > 0:
+            print("NAV:  kx, spd, turnRatio  ", kx, spd, turnRatio)
             navPrint = 0
+        if navPrint == 10:
+            print("NAV:  kx, spd, turnRatio  ", kx, spd, turnRatio)
+            navPrint = 0
+
         navPrint += 1
 
         kx = 0   # req'd to prevent the equiv of a repeat/stuck keyboad key
