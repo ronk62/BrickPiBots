913bbe28d6a533c176e533962e54e968a9d2ca90
ronk62 <ronk62@gmail.com>
2022-10-31 03:24:33 +0000

    from rrwiyatn github with minimal tweaks

---
+++ noBotDevAndSims/rrwiyatn/kalman_filter-102.py
@@ -0,0 +1,153 @@
+'''
+
+from https://github.com/rrwiyatn/robotik/blob/main/kalman_filter/kalman_filter.ipynb
+
+2/27/2022
+
+v102 = tweaking the params
+
+'''
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# state = [x_pos, y_pos]
+num_steps = 10
+ground_truth_xs = np.linspace(0, 10, num=num_steps + 1) # [0, 1, ..., 10]
+ground_truth_ys = ground_truth_xs.copy() # x = y
+ground_truth_states = np.stack((ground_truth_xs,ground_truth_ys), axis=1) # ground_truth_states is [[0,0], [1,1], ..., [10,10]]
+
+
+# Simulate motion with our noisy motion model
+x_0, y_0 = 0, 0 # given initial position at (0,0)
+motion_states = [np.array([x_0, y_0])] # a list to store state at each step following noisy motion model
+u_t = np.array([1.0, 1.0]) # given constant control input
+A = np.array([[1, 0],
+              [0, 1]])
+B = np.array([[1, 0],
+              [0, 1]])
+Q = np.array([[0.3, 0],
+              [0, 0.3]]) # given Q matrix
+for _ in range(10):
+    motion_noise = np.random.multivariate_normal(mean=np.array([0,0]), cov=Q) # ~N(0,Q)
+    # motion_noise = 0 # hard-code noise to 0
+    new_state = A @ motion_states[-1] + B @ u_t + motion_noise # note: a @ b = np.dot(a,b)
+    motion_states.append(new_state)
+    
+    
+# Simulate measurements with our noisy measurement model
+# In real life, we are supposed to get these directly from our sensor
+measurement_states = [np.array([x_0, y_0])] # # a list to store state at each step following noisy measurement model, assume we have perfect initial measurement
+H = np.array([[1, 0],
+              [0, 1]]) # given H matrix
+R = np.array([[0.75, 0],
+              [0, 0.6]]) # given R matrix
+for i in range(10):
+    measurement_noise = np.random.multivariate_normal(mean=np.array([0,0]), cov=R) # ~N(0,R)
+    # measurement_noise = 0 # hard-code noise to 0
+    new_measurement = H @ ground_truth_states[i+1] + measurement_noise # this is z_t
+    measurement_states.append(new_measurement)
+
+    
+# Convert motion_states and measurement_states to array so we can plot them easily
+motion_states = np.array(motion_states)
+measurement_states = np.array(measurement_states)
+
+
+# # Compare ground truth and measurements
+# plt.figure(1)
+# plt.plot(ground_truth_states[:,0], ground_truth_states[:,1])
+# plt.plot(motion_states[:,0], motion_states[:,1])
+# plt.plot(measurement_states[:,0], measurement_states[:,1])
+# plt.xlim(-1,12)
+# plt.ylim(-1,12)
+# plt.xlabel('x position')
+# plt.ylabel('y position')
+# plt.legend(['ground truth', 'motion model', 'measurements'])
+# plt.gca().set_aspect('equal', adjustable='box')
+# plt.show()
+
+
+
+def predict(A, B, Q, u_t, mu_t, Sigma_t):
+    predicted_mu = A @ mu_t + B @ u_t
+    predicted_Sigma = A @ Sigma_t @ A.T + Q
+    return predicted_mu, predicted_Sigma
+
+
+
+def update(H, R, z, predicted_mu, predicted_Sigma):
+    residual_mean = z - H @ predicted_mu
+    residual_covariance = H @ predicted_Sigma @ H.T + R
+    kalman_gain = predicted_Sigma @ H.T @ np.linalg.inv(residual_covariance)
+    updated_mu = predicted_mu + kalman_gain @ residual_mean
+    updated_Sigma = predicted_Sigma - kalman_gain @ H @ predicted_Sigma
+    return updated_mu, updated_Sigma
+
+
+# Re-initialize the problem with the given information
+mu_0 = np.array([0, 0])
+Sigma_0 = np.array([[0.1, 0],
+                     [0, 0.1]])
+u_t = np.array([1, 1]) # we assume constant control input
+
+A = np.array([[1, 0],
+              [0, 1]])
+B = np.array([[1, 0],
+              [0, 1]])
+Q = np.array([[0.8, 0.1],
+              [0.1, 0.8]])
+H = np.array([[1, 0],
+              [0, 1]])
+R = np.array([[0, 0],
+              [0, 0]])
+
+# Initialize empty lists to store the filtered states and measurements for plotting
+measurement_states = []
+filtered_states = []
+
+# Run KF for each time step
+mu_current = mu_0.copy()
+Sigma_current = Sigma_0.copy()
+for i in range(num_steps):
+    
+    # Predict step
+    predicted_mu, predicted_Sigma = predict(A, B, Q, u_t, mu_current, Sigma_current)
+    
+    # Get measurement (in real life, we get this from our sensor)    
+    measurement_noise = np.random.multivariate_normal(mean=np.array([0,0]), cov=R) # ~N(0,R)
+    # measurement_noise = 0 # hard-code noise to 0
+    new_measurement = H @ ground_truth_states[i+1] + measurement_noise # this is z_t
+    
+    # The rest of update step
+    mu_current, Sigma_current = update(H, R, new_measurement, predicted_mu, predicted_Sigma)
+    
+    # Store measurements and mu_current so we can plot it later
+    measurement_states.append(new_measurement)
+    filtered_states.append(mu_current)
+
+print("motion_states = ", motion_states)
+
+print("measurement_states = ", measurement_states)
+
+print("filtered_states = ", filtered_states)
+
+# For plotting purposes, convert the lists to array
+measurement_states = np.array(measurement_states)
+filtered_states = np.array(filtered_states) 
+
+# Let's plot the results
+# plt.figure(2)
+plt.plot(ground_truth_states[:,0], ground_truth_states[:,1]) # from previous section
+plt.plot(motion_states[:,0], motion_states[:,1]) # from previous section
+plt.plot(measurement_states[:,0], measurement_states[:,1])
+plt.plot(filtered_states[:,0], filtered_states[:,1])
+plt.xlim(-1,12)
+plt.ylim(-1,12)
+plt.xlabel('x position')
+plt.ylabel('y position')
+plt.legend(['ground truth', 'motion model', 'measurements', 'KF'])
+plt.gca().set_aspect('equal', adjustable='box')
+plt.show()
